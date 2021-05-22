7722d5ab0778ee7fc2c8e3df4a886d60516d39e1
ee07b505 <51856039+ee07b505@users.noreply.github.com>
2021-05-23 00:47:53 +0800

    Add files via upload

---
+++ address.sh
@@ -0,0 +1,15 @@
+tCnt=`ls  /root/beeconfig/bee*.yaml | wc -l`
+ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v 172|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
+echo "ip,num,dpi_port,address,chequebook" > ./${ip}.txt
+for ((i=1; i<=tCnt; i ++))
+do
+dpi_port=`cat /root/beeconfig/bee${i}.yaml | grep 'debug-api-addr: :' | awk -F ':' '{print $3}'`
+echo "节点${i}的端口为：${dpi_port}"
+echo "检查cashout${i}.sh……"
+echo "节点${i}的钱包地址和合约地址:"
+address=`curl -s localhost:${dpi_port}/addresses | jq .ethereum`
+echo "address:${address}"
+chequebook=`curl -s http://localhost:${dpi_port}/chequebook/address | jq .chequebookaddress`
+echo "chequebook:${chequebook}"
+echo "${ip},${i},${dpi_port},${address},${chequebook}" >> ./${ip}.txt
+done
